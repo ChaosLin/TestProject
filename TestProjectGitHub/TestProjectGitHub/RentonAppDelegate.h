12856201925dfb9ace86349b018cc951decabd2f
Renton Lin <linweichao12@163.com>
2014-05-11 15:03:50 +0800

    init

---
+++ TestProjectGitHub/TestProjectGitHub/RentonAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RentonAppDelegate.h
+//  TestProjectGitHub
+//
+//  Created by Chaos Lin on 5/11/14.
+//  Copyright (c) 2014 Chaos Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RentonAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestProjectGitHub/TestProjectGitHub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestProjectGitHub
+//
+//  Created by Chaos Lin on 5/11/14.
+//  Copyright (c) 2014 Chaos Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RentonAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RentonAppDelegate class]));
+    }
+}
